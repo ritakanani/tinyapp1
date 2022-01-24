19c1ad1b0a65f630bcd49af512dd082ab95092c1
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-01-24 17:40:59 -0500

    install EJS Template

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;  // default port 8080
 
+// //     TEMPLATE    // //
+app.set("view engine", "ejs");  // This tells the Express app to use EJS as its templating engine.
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",

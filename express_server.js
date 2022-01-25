e53f0d6549c4cafa3927e1b5e5b837b169562675
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-01-25 18:45:27 -0500

    POST method for shortURL

--- express_server.js
+++ express_server.js
@@ -82,11 +82,18 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  console.log(longURL);
+  const longURL = urlDatabase[req.params.shortURL];  
   res.redirect(longURL);
 });
+
 
 
 app.listen(PORT, () => {

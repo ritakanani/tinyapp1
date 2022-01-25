24a91fe938488ea6869a938cf5f893d661ba35da
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-01-25 00:16:07 -0500

    POST method for urls_new form file

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080;  // default port 8080
 
+// //       MIDDLEWARE      // //
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 // //       TEMPLATE      // //
 app.set("view engine", "ejs");  // This tells the Express app to use EJS as its templating engine.
 
@@ -9,6 +13,20 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString(length) {
+  
+  let result = "";
+  
+  const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+console.log(generateRandomString(6));
 
 
 // //     ROUTE     // //
@@ -40,12 +58,26 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// //         for NEW FORM file         // //
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("ok");
+
+});
 
 // //        for SHOW file         // //
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
